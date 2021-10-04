483637e75b6b314e5391e2f357e52954a642cb9a
AsaduzzamanSam <samrat.asad@gmail.com>
2021-10-05 05:21:49 +0600

    project UI done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,46 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.white24,
+      appBar: AppBar(
+        title: Center(child: Text('DICE')),
+        backgroundColor: Colors.black26,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
 
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image(
+                image: AssetImage('images/dice5.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
